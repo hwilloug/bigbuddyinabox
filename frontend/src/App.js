ea9fc732db148a96c68d2a83f9732c6a26756a51
Hannah Willoughby <hannah@tcah.com>
2023-05-01 18:18:11 -0400

    Rough display of images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import PhotoAlbum from "react-photo-album"
+import { useCallback } from "react"
+import './App.css'
 
 function App() {
+
+  const size1 = 400
+  const size2 = 700
+
+  const padding = 30
+  const spacing = 30
+
+  const photos = [
+    {src: "IMG_8315.JPG", width: size1, height: size2},
+    {src: "IMG_8320.JPG", width: size1, height: size2},
+    {src: "IMG_8330.JPG", width: size1, height: size2},
+    {src: "IMG_8351.JPG", width: size1, height: size2},
+    {src: "IMG_8363.JPG", width: size1, height: size2},
+    {src: "IMG_8373.JPG", width: size1, height: size2},
+    {src: "IMG_8381.JPG", width: size1, height: size2},
+    {src: "IMG_8384.JPG", width: size2, height: size1},
+    {src: "IMG_8401.JPG", width: size1, height: size2},
+    {src: "IMG_8419.JPG", width: size1, height: size2},
+    {src: "IMG_8429.JPG", width: size1, height: size2},
+    {src: "IMG_8431.JPG", width: size1, height: size2},
+    {src: "IMG_8437.JPG", width: size1, height: size2},
+    {src: "IMG_8443.JPG", width: size2, height: size1},
+    {src: "IMG_8462.JPG", width: size2, height: size1},
+    {src: "IMG_8464.JPG", width: size1, height: size2},
+    {src: "IMG_8473.JPG", width: size1, height: size2},
+    {src: "IMG_8476.JPG", width: size1, height: size2},
+    {src: "IMG_8478.JPG", width: size1, height: size2},
+    {src: "IMG_8479.JPG", width: size1, height: size2},
+    {src: "IMG_8487.JPG", width: size1, height: size2},
+    {src: "IMG_8496.JPG", width: size1, height: size2},
+    {src: "IMG_8497.JPG", width: size1, height: size2},
+    {src: "IMG_8507.JPG", width: size1, height: size2},
+    {src: "IMG_8551.JPG", width: size1, height: size2},
+    {src: "IMG_8581.JPG", width: size1, height: size2},
+    {src: "IMG_8594.JPG", width: size1, height: size2}
+  ]
+
+  const renderPhoto = useCallback(
+    ({ imageProps: { alt, style, ...rest } }) => (
+        <img
+            alt={alt}
+            style={{
+                ...style,
+                borderRadius: "4px",
+                boxShadow: "0px 3px 3px -2px rgb(0 0 0 / 20%), 0px 3px 4px 0px rgb(0 0 0 / 14%), 0px 1px 8px 0px rgb(0 0 0 / 12%)",
+                transition: "box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms",
+                backgroundColor: 'white'
+            }}
+            {...rest}
+        />
+    )
+);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <main>
+        <PhotoAlbum 
+          layout="masonry" 
+          photos={photos}
+          spacing={spacing}
+          padding={padding}
+          renderPhoto={renderPhoto}
+        />
+      </main>
     </div>
   );
 }
