325d9bc476be2b51ecdde9d7933fd69bbcd1f54f
Hannah Willoughby <hannah@tcah.com>
2023-05-05 16:03:44 -0400

    Update gitignore

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -59,7 +59,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <h1>BuddyInABox.com</h1>
+        <h1>BigBuddyInABox.com</h1>
       </header>
       <main>
         <PhotoAlbum 
